f857342932275c69544fd3a502bd09faa77e12ae
Hanqing Liu <lhq3361951@gmail.com>
2022-01-21 11:39:28 -0500

    added add comment function, can save and load comments in firebase

--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,14 +1,23 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from "react";
 
 import useHttp from "../../hooks/use-http";
 import { addComment } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
-import classes from './NewCommentForm.module.css';
+import classes from "./NewCommentForm.module.css";
 
 const NewCommentForm = (props) => {
   const commentTextRef = useRef();
+  
+  const { sendRequest, status, error } = useHttp(addComment);
 
-  const { sendRequest, status} = useHttp(addComment);
+  const { onAddedComment} = props;
+
+  useEffect(() => {
+    if(status === 'completed' && !error) {
+      onAddedComment();
+    }
+  },[status, error, onAddedComment]);
 
   const submitFormHandler = (event) => {
     event.preventDefault();
@@ -16,16 +25,23 @@ const NewCommentForm = (props) => {
     // optional: Could validate here
 
     // send comment to server
+    const enteredText = commentTextRef.current.value;
+    sendRequest({ commentData: { text: enteredText }, quoteId: props.quoteId});
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === "pending" && (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
       </div>
       <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
+        <button className="btn">Add Comment</button>
       </div>
     </form>
   );
